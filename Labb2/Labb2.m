537879d34bdb1eb3d57af1dc92ce066b9013775c
BlixterinoProductions <max.blixt@gmail.com>
2016-11-08 12:51:59 +0100

    l2

--- Labb2/Labb2.m
+++ Labb2/Labb2.m
@@ -7,7 +7,6 @@ if a<b
 else 
     b=c;
 end
-%%
 
 %% Uppgift 2
 format long
@@ -17,17 +16,14 @@ tol=1e-6;
 i=0;
 while d>tol
     xny=x+(-1)^i/(2*i+1);
-    i=i+1
+    i=i+1;
     d=abs(xny-x);
     x=xny
 end
 x
-%%
 
 %% Uppgift 3
-
-%%
+f=polylen
 
 %% Uppgift 4
 
-%%
