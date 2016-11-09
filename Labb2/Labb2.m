bc521ce13df41cbafe6f38ddc96cc41c31b7d9da
dr-fluff <oliver.gustavssons@gmail.com>
2016-11-09 12:36:19 +0100

    uppgift 3

--- Labb2/Labb2.m
+++ Labb2/Labb2.m
@@ -1,4 +1,5 @@
 %% uppgift 1 
+% L?t c vara s? liten som m?jligt
 a=7;
 b=8;
 
@@ -7,9 +8,10 @@ if a<b
 else 
     b=c;
 end
-%%
+
 
 %% Uppgift 2
+% Ber?kna pi/4 med 5 decimaler
 format long
 d=1;
 x=0;
@@ -17,17 +19,24 @@ tol=1e-6;
 i=0;
 while d>tol
     xny=x+(-1)^i/(2*i+1);
-    i=i+1
+    i=i+1;
     d=abs(xny-x);
     x=xny
 end
 x
-%%
-
 %% Uppgift 3
 
-%%
+n=length(x);
+L=0;
+for i=1:n-1
+    L=L+sqrt((x(i+1)-x(i))^2+(y(i+1)-y(i))^2);
+end
+L
+
 
 %% Uppgift 4
-
-%%
+%Mata in punkter med ginput s?a att du f?ar ett polygont?ag som du sedan sluter.
+%Du skall inte best?amma antal punkter i f?orv?ag, men det skall vara fler punkter ?an i en triangel.
+%F?argl?agg sedan omr?adet som innesluts med n?agon f?arg. G?a till Help och l?as i hj?alptexten f?or
+%ginput s?a du kan ta reda p?a hur man kan l?asa in koordinater f?or flera punkter, utan att i f?orv?ag
+%beh?ova best?amma hur m?anga
