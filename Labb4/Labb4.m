e3cec44baf6a3c6d9c931f4359bcc680756acb2c
dr-fluff <oliver.gustavssons@gmail.com>
2016-11-18 13:51:51 +0100

    Labb 4 uppgift 1

--- Labb4/Labb4.m
+++ Labb4/Labb4.m
@@ -1,24 +1,37 @@
 %% Uppgift 1
 %% 1a)
-A=[1 3; 5 -2]; 
-B=[6 13];
+A=[1 3; 5 -2]; b=[6 13];
 
+l1=@(x1)(b(1)-A(1,1)*x1)/A(1,2);
+l2=@(x1)(b(2)-A(2,1)*x1)/A(2,2);
+
+x1=linspace(0,5,5);
+plot(x1,l1(x1),'b','linewidth',2), hold on
+plot(x1,l2(x1),'g','linewidth',2)
+xlabel('x_1'), ylabel('x_2')
+axis equal, axis([1 5 0 3])
+grid
 
 %% 1b)
-A=[1 3; 2 6];
-B1=[5 12];
-B2=[5 10];
+%EJ f?rdig
+A=[1 3; 2 6]; b1=[5 12]; b2=[5 10];
 
+l1=@(x1)(b1(1)-A(1,1)*x1)/A(1,2);
+l2=@(x1)(b1(2)-A(2,1)*x1)/A(2,2);
+l4=@(x1)(b2(2)-A(2,1)*x1)/A(2,2);
+
+x1=linspace(0,5,5);
+plot(x1,l1(x1),'r','linewidth',2), hold on
+plot(x1,l2(x1),'g','linewidth',2)
+plot(x1,l4(x1),'b','linewidth',2)
+xlabel('x_1'), ylabel('x_2')
+axis equal, axis([-5 10 -5 10])
+grid
 
 %% Uppgift 2
 %% 2a)
-A=[-3 1 2; 3 -2 1; -1 5 1];
-B=[2; 1; 1];
+A=[-3 1 2; 3 -2 1; -1 5 1]; B=[2; 1; 1];
 
 
 %% 2b)
-A=[-3 1 2; 3 -2 1; 3 -4 7];
-B1=[2; 1; 1];
-B2=[2; 1; 7];
-
-s
+A=[-3 1 2; 3 -2 1; 3 -4 7]; B1=[2; 1; 1]; B2=[2; 1; 7];
