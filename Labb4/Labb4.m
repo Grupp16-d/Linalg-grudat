80297347d1ea2abfc3e1346c00ce12196f499cf1
dr-fluff <oliver.gustavssons@gmail.com>
2016-11-18 14:11:58 +0100

    uppgift 2 början

--- Labb4/Labb4.m
+++ Labb4/Labb4.m
@@ -32,6 +32,15 @@ grid
 %% 2a)
 A=[-3 1 2; 3 -2 1; -1 5 1]; B=[2; 1; 1];
 
+l1=@(x1,x2)(b(1)-A(1,1)*x1-A(1,2)*x2)/A(1,3);
+l2=@(x1,x2)(b(2)-A(2,1)*x1-A(2,2)*x2)/A(2,3);
+l3=@(x1,x2)(b(3)-A(3,1)*x1-A(3,2)*x2)/A(3,3);
+x1=linspace(-1,1,30); x2=linspace(-1,1,30);
+[X1,X2]=meshgrid(x1,x2);
+surf(X1,X2,l1(X1,X2),'facecolor',''), hold on
+xlabel('x_1'), ylabel('x_2'), zlabel('x_3')
+axis([-1 1 -1 1 -4 7]), axis vis3d, grid on, box on
+
 
 %% 2b)
 A=[-3 1 2; 3 -2 1; 3 -4 7]; B1=[2; 1; 1]; B2=[2; 1; 7];
