9a043ab582a64d42aeb653ca5b099279fd1cc8e0
BlixterinoProductions <max.blixt@gmail.com>
2016-11-28 10:53:44 +0100

    labb 4

--- Labb4/Labb4.m
+++ Labb4/Labb4.m
@@ -1,6 +1,6 @@
 %% Uppgift 1
 %% 1a)
-A=[1 3; 5 -2]; b=[6 13];
+A=[1 3; 5 -2]; b=[6; 13];
 
 l1=@(x1)(b(1)-A(1,1)*x1)/A(1,2);
 l2=@(x1)(b(2)-A(2,1)*x1)/A(2,2);
@@ -11,6 +11,12 @@ plot(x1,l2(x1),'g','linewidth',2)
 xlabel('x_1'), ylabel('x_2')
 axis equal, axis([1 5 0 3])
 grid
+
+R=rref([A b]);
+
+x=[R(1, 3); R(2, 3)];
+plot(x(1),x(2),'ro','markersize',3,'linewidth',4), hold off
+
 
 %% 1b) B1
 A=[1 3; 2 6]; b1=[5 12];
