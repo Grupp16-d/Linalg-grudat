1a6595927917dac0e1630c2b1afdf168f6115972
BlixterinoProductions <max.blixt@gmail.com>
2016-11-28 11:14:24 +0100

    s

--- Labb4/Labb4.m
+++ Labb4/Labb4.m
@@ -19,20 +19,25 @@ plot(x(1),x(2),'ro','markersize',3,'linewidth',4), hold off
 
 
 %% 1b) B1
-A=[1 3; 2 6]; b1=[5 12];
+A=[1 3; 2 6]; b1=[5; 12];
 
 l1=@(x1)(b1(1)-A(1,1)*x1)/A(1,2);
 l2=@(x1)(b1(2)-A(2,1)*x1)/A(2,2);
 
 x1=linspace(0,5,5);
-plot(x1,l1(x1),'r','linewidth',2), hold on
+plot(x1,l1(x1),'r','linewidth',4), hold on
 plot(x1,l2(x1),'g','linewidth',2)
 xlabel('x_1'), ylabel('x_2')
 axis equal, axis([-10 10 -10 10])
 grid
 
+R=rref([A b1]);
+
+x=[R(1, 3); R(2, 3)];
+plot(x(1),x(2),'ro','markersize',3,'linewidth',4), hold off
+
 %% A och B2
-A=[1 3; 2 6]; b=[5 10]; 
+A=[1 3; 2 6]; b2=[5; 10]; 
 
 l1=@(x1)(b(1)-A(1,1)*x1)/A(1,2);
 l2=@(x1)(b(2)-A(2,1)*x1)/A(2,2);
@@ -44,6 +49,11 @@ xlabel('x_1'), ylabel('x_2')
 axis equal, axis([-10 10 -10 10])
 grid
 
+R=rref([A b2]);
+
+x=[R(1, 3); R(2, 3)];
+plot(x(1),x(2),'ro','markersize',3,'linewidth',4), hold off
+
 %% Uppgift 2
 %% 2a)
 A=[-3 1 2;3 -2 1; -1 5 -1]; b=[2;1;1];
@@ -54,9 +64,15 @@ l3=@(x1,x2)(b(3)-A(3,1)*x1-A(3,2)*x2)/A(3,3);
 x1=linspace(-1,1,30); x2=linspace(-1,1,30);
 [X1,X2]=meshgrid(x1,x2);
 surf(X1,X2,l1(X1,X2),'facecolor','b'), hold on
+surf(X1,X2,l2(X1,X2),'facecolor','g')
+surf(X1,X2,l3(X1,X2),'facecolor','c')
 xlabel('x_1'), ylabel('x_2'), zlabel('x_3')
 axis([-1 1 -1 1 -4 7]), axis vis3d, grid on, box on
-surf(X1,X2,l2(X1,X2),'facecolor','g')
+
+R=rref([A(1:2,:) b(1:2)])
+xt=@(t)[-1.6667+1.6667*t;-3+3*t;t]
+P=[xt(0) xt(2)];
+plot3(P(1,:),P(2,:),P(3,:),'r','linewidth',3), hold off
 
 %% 2b) A och b1
 A=[-3 1 2; 3 -2 1; 3 -4 7]; b=[2; 1; 1];
