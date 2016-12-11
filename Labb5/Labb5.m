39928b8cb9a8a36b276ba213aca264a1259530c6
dr-fluff <oliver.gustavssons@gmail.com>
2016-12-11 13:09:36 +0100

    yo
    ad

--- Labb5/Labb5.m
+++ Labb5/Labb5.m
@@ -93,11 +93,41 @@ plot3(spegling(1),spegling(2),spegling(3),'ro')
 myquiv(npkt,-proj,1,'m')
 figure(gcf)
 %% Uppgift 5
-%Rita samma plan som uppgift 4
-xmin=-2; xmax=2; ymin=-2; ymax=2;
+clear all
+close all
+clc
+
+%rita planet
+xmin=-4; xmax=4; ymin=-4; ymax=4;
 a=1; b=-1; c=4; d=1;
 X=[xmin xmax xmax xmin]; Y=[ymin ymin ymax ymax];
 Z=(d-a*X-b*Y)/c;
-fill3(X,Y,Z,'g','facealpha',0.7)
+fill3(X,Y,Z,'g','facealpha',0.7), hold on
 xlabel('x'), ylabel('y')
-axis equal, grid on
+figure(gcf)
+
+%Rita ut normalen
+pkt=[0 0 1/4];
+normalen=[a b c];
+quiver3(pkt(1),pkt(2),pkt(3),normalen(1),normalen(2),normalen(3),1/2)
+myquiv=@(x,y,s,str)quiver3(x(1),x(2),x(3),y(1),y(2),y(3),s,str);
+figure(gcf)
+
+% rita ut kuben
+H=[0 1 0 1 0 1 0 1; 0 0 1 1 0 0 1 1; 0 0 0 0 1 1 1 1];
+Hp = [H+ones(3,8)];
+S=[1 2 4 3; 1 2 6 5; 1 3 7 5; 3 4 8 7; 2 4 8 6; 5 6 8 7];
+
+figure(gcf)
+
+P= Hp;
+
+for i=1:size(S,1)
+    Si=S(i,:); 
+    fill3(P(1,Si),P(2,Si),P(3,Si),'r','facealpha',0.5)
+end
+
+ax = [-4 4 -4 4 -4 4];
+axis equal, axis(ax);
+box on, grid on;
+
