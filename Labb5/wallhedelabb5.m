9c711b9c5151c954027c33c1bba6c208178cbd78
dr-fluff <oliver.gustavssons@gmail.com>
2016-12-11 14:37:26 +0100

    labb 5 uppgift 4 och 5

--- Labb5/wallhedelabb5.m
+++ Labb5/wallhedelabb5.m
@@ -1,3 +1,35 @@
+%% Uppgift 4
+xmin=-2; xmax=2; ymin=-2; ymax=2;
+a=1; b=-1; c=4; d=1;
+X=[xmin xmax xmax xmin]; Y=[ymin ymin ymax ymax];
+Z=(d-a*X-b*Y)/c;
+fill3(X,Y,Z,'g','facealpha',0.7),hold on
+xlabel('x'), ylabel('y')
+axis equal, grid on 
+
+U = quiver3(Z(1),Z(1),Z(1),a,b,c,0.2)
+text(0.5,0,1,'n')
+
+n = [a b c];
+x=[1 1 1] ;
+plot3(x(1),x(2),x(3),'ro');
+text(x(1)+0.1,x(2),x(3),'x');
+
+alph = (d-dot(n,x))/dot(n,n);
+xo = x+alph*n;
+plot3(xo(1),xo(2),xo(3),'ro');
+v=[xo;x];
+plot3(v(:,1),v(:,2),v(:,3),'r');
+text(xo(1)+0.1,xo(2),xo(3),'x_o');
+
+xr = x + 2*alph*n;
+plot3(xr(1),xr(2),xr(3),'ro');
+u=[xr;x];
+plot3(u(:,1),u(:,2),u(:,3),'r');
+text(xr(1)+0.1,xr(2),xr(3),'x_r');
+hold off
+
+%%
 %rita planet
 xmin=-4; xmax=4; ymin=-4; ymax=4;
 a=1; b=-1; c=4; d=1;
