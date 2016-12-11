e5695255d100bbd0a6ec749d06416ba79db4ce28
dr-fluff <oliver.gustavssons@gmail.com>
2016-12-11 15:10:33 +0100

    yo-yo

--- Labb6/Labb6.m
+++ Labb6/Labb6.m
@@ -1,18 +1,5 @@
 %% Uppgift 1
 
-td=[5 6 7 8 9 10];
-yd=[19.5888 23.4043 25.5754 29.1231 31.9575 35.8116;]; 
-A=[ones(size(td)) td];
-x=A\yd; a=x(1); b=x(2);
-n=length(td);
-e=norm(A*x-yd)/sqrt(n);
-
-x1=linspace(0,5,5);
-fx=@(x1)(a+b*x1);
-plot((x1),fx(x1),'ro','markersize',3,'linewidth',4), hold off
-plotmatrix(td, )
-
-
 %% 1b)
 
 %% Uppgift 2
