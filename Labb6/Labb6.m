71391e564e17e329f04068b2d23010c0f971adf7
dr-fluff <oliver.gustavssons@gmail.com>
2016-12-11 15:19:27 +0100

    uppgift 2

--- Labb6/Labb6.m
+++ Labb6/Labb6.m
@@ -11,3 +11,12 @@ plot(td,y)
 for i=1:size(td)
     
 %% Uppgift 2
+td=[1 2 3 4 5 6 7 8 9 10 11 12]';
+yd=[-0.9 -0.9 2.0 6.0 11.6 15.5 16.6 16.2 12.8 9.1 4.4 1.0]';
+
+a=pi/6;
+
+A=[ones(size(td)) sin(a*td)cos(a*td)];
+x=A\yd;
+n=length(td);
+e=norm(A*x-yd)/sqrt(n);
